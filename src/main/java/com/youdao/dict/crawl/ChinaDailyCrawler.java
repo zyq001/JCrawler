4db4c4f6637635431485c0dc617fca29e337fac8
LiuHongliang <liuhongliang@corp.youdao.com>
2015-10-13 10:21:34 +0800

    FIX keywords

--- src/main/java/com/youdao/dict/crawl/ChinaDailyCrawler.java
+++ src/main/java/com/youdao/dict/crawl/ChinaDailyCrawler.java
@@ -199,7 +199,7 @@ public class ChinaDailyCrawler extends DeepCrawler {
 //        crawler.setResumable(true);
         crawler.setResumable(false);
 
-        crawler.start(2);
+        crawler.start(3);
     }
 
 }
