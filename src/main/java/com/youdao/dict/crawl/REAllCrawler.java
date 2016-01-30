20dd13c12492f4cae32a2a4a8fccd2397f020b05
Yaqiang Zang <zangyq@rd.netease.com>
2016-01-30 14:56:40 +0800

    start(1) for Allin

--- src/main/java/com/youdao/dict/crawl/REAllCrawler.java
+++ src/main/java/com/youdao/dict/crawl/REAllCrawler.java
@@ -176,7 +176,7 @@ public class REAllCrawler extends DeepCrawler {
 //        crawler.setResumable(true);
         crawler.setResumable(false);
 
-        crawler.start(2);
+        crawler.start(1);
     }
 
 }
